6fb683ff1e4272463e45a829229cf6767082782c
Alex Nolley <alex@ticenia.(none)>
2010-02-02 13:31:05 -0500

    added toFasta script

--- sqeaqr/dbEntries.py
+++ sqeaqr/dbEntries.py
@@ -2,8 +2,5 @@
 
 import UserDict
 
-# [AN] compress to one tuple/type of db
-FASTQFIELDS = ('id', 'name', 'sequence', 'accuracy')
 FASTQFIELDTYPES = (('name', 'text'), ('sequence', 'text'), ('accuracy', 'text'))
-FASTAFIELDS = ('id', 'name', 'description', 'sequence')
 FASTAFIELDTYPES = (('name', 'text'), ('description', 'text'), ('sequence', 'text'))
---
+++ sqeaqr/toFasta.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import sqeaqrDB
+import dbEntries
+import sys, os
+
+_MAXSEQLINELEN = 80
+
+class _seq_iter(object):
+    """
+    Yields string of characters of _MAXSEQLINELEN characters in length
+    from a given input string
+    """
+    def __init__(self, sequence):
+        self.seq = sequence
+        self.len = len(sequence)
+        self.begin = 0
+        self.end = 0
+        self.step = _MAXSEQLINELEN
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.begin == self.len:
+            raise StopIteration
+
+        self.end += self.step
+        if self.end > self.len:
+            self.end = self.len
+
+        res = self.seq[self.begin:self.end]
+        self.begin = self.end
+        return res
+
+def toFasta(dbFile, outputFile):
+    outFile = open(outputFile, 'wb')
+    db = sqeaqrDB.sqeaqrDB(dbFile)
+
+    for value in db.itervalues():
+        outFile.write('>%s %s\n' % (value['name'], value['description']))
+        # Write the sequence as multiple if longer than self.step
+        seq = _seq_iter(value['sequence'])
+        for line in seq:
+            outFile.write('%s\n' % line)
+    
+    db.close()
+    outFile.close()
+    return
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print "Usage: %s <dbfilename> <outputfilename>" % sys.argv[0]
+        exit(1)
+
+    dbFile = sys.argv[1]
+    outputFile = sys.argv[2]
+
+    if not os.path.isfile(dbFile):
+        print "No such file: %s" % dbFile
+        exit(1)
+    if os.path.isfile(outputFile):
+        os.unlink(outputFile)
+    
+    toFasta(dbFile, outputFile)
