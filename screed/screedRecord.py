baad2be9be6468bdce471c7959980108e45559db
Alex Nolley <badmit@gmail.com>
2010-02-26 13:19:19 -0500

    and here it is

---
+++ screed/screedRecord.py
@@ -0,0 +1,23 @@
+import UserDict
+
+class _screed_record_dict(UserDict.DictMixin):
+    """
+    Simple dict-like record interface with bag behavior.
+    """
+    def __init__(self, *args, **kwargs):
+        self.d = dict(*args, **kwargs)
+        
+    def __getitem__(self, name):
+        return self.d[name]
+
+    def __setitem__(self, name, value):
+        self.d[name] = value
+    
+    def __getattr__(self, name):
+        try:
+            return self.d[name]
+        except KeyError:
+            raise AttributeError, name
+
+    def keys(self):
+        return self.d.keys()
