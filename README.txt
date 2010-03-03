6c28cdc71f9076487435e263dbf13f55b453c105
Alex Nolley <alex@ticenia.(none)>
2010-03-03 15:47:04 -0500

    helpful to actually add the files

---
+++ README.txt
@@ -0,0 +1,1 @@
+See doc/screed.html for documentation
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='screed',
+      version='0.5',
+      description='A short read database',
+      author='Alex Nolley',
+      author_email='badmit@gmail.com',
+      url='http://lyorn.idyll.org/nolleyal/screed/',
+      packages=['screed'],
+      license='BSD'
+      )
