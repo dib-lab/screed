238f0fbe11bb9ba8f22901844a861131edea0757
Alex Nolley <alex@ticenia.(none)>
2010-03-12 16:10:13 -0500

    integrated the 'hava' sequence creation

--- screed/__init__.py
+++ screed/__init__.py
@@ -349,83 +349,83 @@ def create_db(filepath, fields, rcrditer):
     con.commit()
     con.close()
 
-class createdb(object):
-    """
-    Class used for creating screed databases. Records are passed to the object
-    via the feed method. When finished, the object must be closed
-    """
-    def __init__(self, filepath, attributes):
-        """
-        Opens a new screed database at the given filepath. Attributes is a tuple
-        containing the names and order of atrributes per record
-        """
-        if not filepath.endswith(dbConstants.fileExtension):
-            filepath += dbConstants.fileExtension
+## class createdb(object):
+##     """
+##     Class used for creating screed databases. Records are passed to the object
+##     via the feed method. When finished, the object must be closed
+##     """
+##     def __init__(self, filepath, attributes):
+##         """
+##         Opens a new screed database at the given filepath. Attributes is a tuple
+##         containing the names and order of atrributes per record
+##         """
+##         if not filepath.endswith(dbConstants.fileExtension):
+##             filepath += dbConstants.fileExtension
 
-        if os.path.exists(filepath): # Remove existing files
-            os.unlink(filepath)
+##         if os.path.exists(filepath): # Remove existing files
+##             os.unlink(filepath)
 
-        self._attributes = attributes
-        self._con = sqlite3.connect(filepath)
-        self._c = self._con.cursor()
-        self._queryby = self._attributes[0] # Key is first attribute
-        self._makeadmintable()
-        self._makedicttable()
-        self._open = True
+##         self._attributes = attributes
+##         self._con = sqlite3.connect(filepath)
+##         self._c = self._con.cursor()
+##         self._queryby = self._attributes[0] # Key is first attribute
+##         self._makeadmintable()
+##         self._makedicttable()
+##         self._open = True
 
-    def _tocreatestub(self):
-        """
-        Parses self._attributes into an SQL stub to be inserted into statements
-        """
-        sqlList = []
-        for attribute in self._attributes:
-            sqlList.append('%s TEXT' % attribute)
-            sqlList.append(', ')
-        sqlList.pop()
-        return "".join(sqlList)
+##     def _tocreatestub(self):
+##         """
+##         Parses self._attributes into an SQL stub to be inserted into statements
+##         """
+##         sqlList = []
+##         for attribute in self._attributes:
+##             sqlList.append('%s TEXT' % attribute)
+##             sqlList.append(', ')
+##         sqlList.pop()
+##         return "".join(sqlList)
 
-    def _makedicttable(self):
-        """
-        Method to setup the dictionary table for storing records
-        """
-        # Make the table
-        self._c.execute('CREATE TABLE %s (%s INTEGER PRIMARY KEY, %s)' %
-                        (dbConstants._DICT_TABLE, dbConstants._PRIMARY_KEY,
-                         self._tocreatestub()))
-        # Make the index
-        self._c.execute('CREATE UNIQUE INDEX %sidx ON %s(%s)' %
-                        (self._queryby, dbConstants._DICT_TABLE,
-                         self._queryby))
+##     def _makedicttable(self):
+##         """
+##         Method to setup the dictionary table for storing records
+##         """
+##         # Make the table
+##         self._c.execute('CREATE TABLE %s (%s INTEGER PRIMARY KEY, %s)' %
+##                         (dbConstants._DICT_TABLE, dbConstants._PRIMARY_KEY,
+##                          self._tocreatestub()))
+##         # Make the index
+##         self._c.execute('CREATE UNIQUE INDEX %sidx ON %s(%s)' %
+##                         (self._queryby, dbConstants._DICT_TABLE,
+##                          self._queryby))
 
-    def _makeadmintable(self):
-        """
-        Method to setup the admin table containing attribute information
-        """
-        self._c.execute('CREATE TABLE %s (ID INTEGER PRIMARY KEY, '\
-                        'FIELDNAME TEXT)' % dbConstants._SCREEDADMIN)
+##     def _makeadmintable(self):
+##         """
+##         Method to setup the admin table containing attribute information
+##         """
+##         self._c.execute('CREATE TABLE %s (ID INTEGER PRIMARY KEY, '\
+##                         'FIELDNAME TEXT)' % dbConstants._SCREEDADMIN)
 
-        query = 'INSERT INTO %s (FIELDNAME) VALUES (?)' % \
-                dbConstants._SCREEDADMIN
-        for attribute in self._attributes:
-            self._c.execute(query, (attribute,))
+##         query = 'INSERT INTO %s (FIELDNAME) VALUES (?)' % \
+##                 dbConstants._SCREEDADMIN
+##         for attribute in self._attributes:
+##             self._c.execute(query, (attribute,))
 
-    def feed(self, dictobj):
-        """
-        Method for storing records. dictobj is a dictionary object with
-        name->value pairs of record attributes
-        """
-        qmarks = ('?, ' * len(self._attributes))[:-2]
-        attributes = (''.join(['%s, ' % attr for attr in self._attributes]))[:-2]
-        sub = tuple([dictobj[key] for key in self._attributes])
-        query = 'INSERT INTO %s (%s) VALUES (%s)' %\
-                (dbConstants._DICT_TABLE, attributes, qmarks)
-        self._c.execute(query, sub)
+##     def feed(self, dictobj):
+##         """
+##         Method for storing records. dictobj is a dictionary object with
+##         name->value pairs of record attributes
+##         """
+##         qmarks = ('?, ' * len(self._attributes))[:-2]
+##         attributes = (''.join(['%s, ' % attr for attr in self._attributes]))[:-2]
+##         sub = tuple([dictobj[key] for key in self._attributes])
+##         query = 'INSERT INTO %s (%s) VALUES (%s)' %\
+##                 (dbConstants._DICT_TABLE, attributes, qmarks)
+##         self._c.execute(query, sub)
 
-    def close(self):
-        """
-        Closes the object and the sqlite database handle
-        """
-        if self._open:
-            self._con.commit()
-            self._con.close()
-            self._open = False
+##     def close(self):
+##         """
+##         Closes the object and the sqlite database handle
+##         """
+##         if self._open:
+##             self._con.commit()
+##             self._con.close()
+##             self._open = False
--- screed/seqparse.py
+++ screed/seqparse.py
@@ -7,33 +7,38 @@ screed databases. An example 'hava' parser is included
 
 import dbConstants
 import os
-from __init__ import createdb
-#import createdb
+from __init__ import create_db
 
 # Parser for the fake 'hava' sequence
 def read_hava_sequences(filename):
     """
     Function to parse text from the given HAVA file into a screed database
     """
+    def havaiter(handle):
+        """
+        Iterator over a 'hava' sequence file, returning records
+        """
+        data = {}
+        line = handle.readline().strip()
+        while line:
+            data['hava'] = line
+            data['quarzk'] = handle.readline().strip()
+            data['muchalo'] = handle.readline().strip()
+            data['fakours'] = handle.readline().strip()
+            data['selimizicka'] = handle.readline().strip()
+            data['marshoon'] = handle.readline().strip()
+
+            line = handle.readline().strip()
+            yield data
+
+    fields = ('hava', 'quarzk', 'muchalo', 'fakours', 'selimizicka', 'marshoon')
+        
     # Will raise an exception if the file doesn't exist
     theFile = open(filename, "rb")
 
-    fields = ('hava', 'quarzk', 'muchalo', 'fakours', 'selimizicka', 'marshoon')
-    haCreate = createdb(filename, fields)
+    # Setup the iterator function
+    iterfunc = havaiter(theFile)
 
-    # Parse text and add to database
-    nextChar = theFile.read(1)
-    while nextChar != '':
-        data = {}
-        data['hava'] = nextChar + theFile.readline().strip()
-        data['quarzk'] = theFile.readline().strip()
-        data['muchalo'] = theFile.readline().strip()
-        data['fakours'] = theFile.readline().strip()
-        data['selimizicka'] = theFile.readline().strip()
-        data['marshoon'] = theFile.readline().strip()
-
-        haCreate.feed(data)
-        nextChar = theFile.read(1)
-
+    # Create the screed db
+    create_db(filename, fields, iterfunc)
     theFile.close()
-    haCreate.close()
