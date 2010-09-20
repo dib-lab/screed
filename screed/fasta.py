27e5944c7fa4195a62877f9f4fd4ac85d8e2ce6e
C. Titus Brown <titus@idyll.org>
2010-09-20 00:27:35 -0400

    make description parsing in FASTA optional

--- screed/fasta.py
+++ screed/fasta.py
@@ -4,7 +4,7 @@ FieldTypes = (('name', DBConstants._INDEXED_TEXT_KEY),
               ('description', DBConstants._STANDARD_TEXT),
               ('sequence', DBConstants._SLICEABLE_TEXT))
 
-def fasta_iter(handle):
+def fasta_iter(handle, parse_description=True):
     """
     Iterator over the given FASTA file handle, returning records. handle
     is a handle to a file opened for reading
@@ -17,10 +17,13 @@ def fasta_iter(handle):
         if not line.startswith('>'):
             raise IOError("Bad FASTA format: no '>' at beginning of line")
 
-        # Try to grab the name and optional description
-        try:
-            data['name'], data['description'] = line[1:].split(' ', 1)
-        except ValueError: # No optional description
+        if parse_description: # Try to grab the name and optional description
+            try:
+                data['name'], data['description'] = line[1:].split(' ', 1)
+            except ValueError: # No optional description
+                data['name'] = line[1:]
+                data['description'] = ''
+        else:
             data['name'] = line[1:]
             data['description'] = ''
 
