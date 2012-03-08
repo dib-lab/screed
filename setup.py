ec96042d53947b13681fe63c8be5aae51640eebb
C. Titus Brown <titus@idyll.org>
2012-03-08 15:05:24 -0800

    bumped version #

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='screed',
-      version='0.5',
+      version='0.7',
       description='A short read database',
-      author='Alex Nolley',
-      author_email='badmit@gmail.com',
-      url='http://github.com/acr/screed/',
+      author='Alex Nolley, C. Titus Brown',
+      author_email='badmit@gmail.com, ctb@msu.edu',
+      url='http://github.com/ctb/screed/',
       packages=['screed', 'screed.tests'],
       package_data={'screed.tests': ['test.*', 'test-whitespace.fa']},
       license='BSD',
