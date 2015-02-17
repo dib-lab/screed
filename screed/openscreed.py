66d189a4c82ccd9307a6403777814e93a75828a1
Luiz Irber <luiz.irber@gmail.com>
2015-02-17 16:51:08 -0500

    Fix str/bytes on file detection

--- screed/openscreed.py
+++ screed/openscreed.py
@@ -40,8 +40,8 @@ def open_reader(filename, *args, **kwargs):
     Deals with .gz, FASTA, and FASTQ records.
     """
     magic_dict = {
-        "\x1f\x8b\x08": "gz",
-        "\x42\x5a\x68": "bz2",
+        b"\x1f\x8b\x08": "gz",
+        b"\x42\x5a\x68": "bz2",
         # "\x50\x4b\x03\x04": "zip"
     }  # Inspired by http://stackoverflow.com/a/13044946/1585509
     bufferedfile = io.open(file=filename, mode='rb', buffering=8192)
@@ -67,12 +67,19 @@ def open_reader(filename, *args, **kwargs):
 
     iter_fn = None
     try:
-        if peek[0] == '>':
-            iter_fn = fasta_iter
-        elif peek[0] == '@':
-            iter_fn = fastq_iter
+        first_char = peek[0]
     except IndexError as err:
         return []  # empty file
+
+    try:
+        first_char = chr(first_char)
+    except TypeError:
+        pass
+
+    if first_char == '>':
+        iter_fn = fasta_iter
+    elif first_char == '@':
+        iter_fn = fastq_iter
 
     if iter_fn is None:
         raise ValueError("unknown file format for '%s'" % filename)
