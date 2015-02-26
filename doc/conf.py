22f5a1e62ad20646916339192c15ebf066f66ac9
Jake Fenton <bocajnotnef@gmail.com>
2015-02-26 10:25:08 -0500

    Updated copyright info

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'screed'
-copyright = u'2015, Alex Nolley and Titus Brown'
+copyright = u'2012-2015, Michigan State University'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
