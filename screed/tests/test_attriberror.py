9fb9eec978af055b4f44346d8c06fe7586a8e345
Jake Fenton <bocajnotnef@gmail.com>
2015-04-01 19:07:21 -0400

    changed test to test screedDB not just screed reads

--- screed/tests/test_attriberror.py
+++ screed/tests/test_attriberror.py
@@ -1,18 +1,20 @@
 import screed
 import os
+import ipdb
 
 class nostring:
     def __init__(self):
         self.exists = True
     def __str__(self):
-        raise AttributeError
+        raise Exception
     def __repr__(self):
-        raise AttributeError
+        raise Exception
 
 def test_eq():
     testfile = os.path.join(os.path.dirname(__file__), 'test.fa')
     ns = nostring()
 
     for r in screed.open(testfile):
-        r.sequence == ns
+        r.sequence.__eq__(ns)
+        # # # # # # # # ipdb.set_trace()
 
