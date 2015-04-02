dabe309350efa2dcdc53f6213a2c7de4a783b722
Jake Fenton <bocajnotnef@gmail.com>
2015-04-01 21:31:45 -0400

    added more tests, changed to class format, made tests clean up after themselves, formatting

--- screed/tests/test_attriberror.py
+++ screed/tests/test_attriberror.py
@@ -1,44 +1,87 @@
 import screed
+from screed.DBConstants import fileExtension
 import os
+
 
 class nostring:
     def __init__(self):
         self.exists = True
+
     def __str__(self):
         raise AttributeError
+
     def __repr__(self):
         raise AttributeError
 
-def test_comparisons():
-    testfile = os.path.join(os.path.dirname(__file__), 'test.fa')
-    screed.read_fasta_sequences(testfile)
-    
-    db = screed.ScreedDB(testfile)
-    ns = nostring()
 
-    for k in db:
-        record = db.get(k)
-        try:
-            res = (record.sequence == ns)
-        except TypeError:
-            assert True
-        except:
-            assert False, "should have caught the TypeError"
+class test_comparisons():
 
-    for k in db:
-        record = db.get(k)
-        try:
-            res = (record.sequence != ns)
-        except TypeError:
-            assert True
-        except:
-            assert False, "should have caught the TypeError"
+    def setup(self):
+        self._testfile = os.path.join(os.path.dirname(__file__), 'test.fa')
+        screed.read_fasta_sequences(self._testfile)
 
-    for k in db:
-        record = db.get(k)
-        try:
-            res = (record.sequence >= ns)
-        except TypeError:
-            assert True
-        except:
-            assert False, "should have caught the TypeError"
+        self._db = screed.ScreedDB(self._testfile)
+        self._ns = nostring()
+
+    def test_eq(self):
+        for k in self._db:
+            record = self._db.get(k)
+            try:
+                res = (record.sequence == self._ns)
+            except TypeError:
+                assert True
+            except:
+                assert False, "should have caught the TypeError"
+
+    def test_neq(self):
+        for k in self._db:
+            record = self._db.get(k)
+            try:
+                res = (record.sequence != self._ns)
+            except TypeError:
+                assert True
+            except:
+                assert False, "should have caught the TypeError"
+
+    def test_comp_greateq(self):
+        for k in self._db:
+            record = self._db.get(k)
+            try:
+                res = (record.sequence >= self._ns)
+            except TypeError:
+                assert True
+            except:
+                assert False, "should have caught the TypeError"
+
+    def test_comp_lesseq(self):
+        for k in self._db:
+            record = self._db.get(k)
+            try:
+                res = (record.sequence <= self._ns)
+            except TypeError:
+                assert True
+            except:
+                assert False, "should have caught the TypeError"
+
+    def test_comp_less(self):
+        for k in self._db:
+            record = self._db.get(k)
+            try:
+                res = (record.sequence < self._ns)
+            except TypeError:
+                assert True
+            except:
+                assert False, "should have caught the TypeError"
+
+    def test_comp_great(self):
+        for k in self._db:
+            record = self._db.get(k)
+            try:
+                res = (record.sequence > self._ns)
+            except TypeError:
+                assert True
+            except:
+                assert False, "should have caught the TypeError"
+
+    def teardown(self):
+        os.unlink(self._testfile + fileExtension)
