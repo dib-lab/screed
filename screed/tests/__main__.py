19c065775ba23eb5cdfc410e5b366923f840a812
Daniel Standage <daniel.standage@gmail.com>
2016-12-19 12:01:19 -0800

    Debug

--- screed/tests/__main__.py
+++ screed/tests/__main__.py
@@ -4,4 +4,5 @@ if __name__ == '__main__':
     import pytest
     errno = pytest.main(['-m', '"not known_failing"', '-v'])
     #errno = pytest.main()
+    print('DEBUG', file=sys.stderr)
     sys.exit(errno)
