5f009825161b6e7c88601e8778f18c927b2fe78f
Daniel Standage <daniel.standage@gmail.com>
2016-12-19 11:52:11 -0800

    Hard-coded pytest args

--- screed/tests/__main__.py
+++ screed/tests/__main__.py
@@ -2,6 +2,6 @@ import sys
 
 if __name__ == '__main__':
     import pytest
-#    errno = pytest.main(['-m', '"not known_failing"', '-v'])
-    errno = pytest.main()
+    errno = pytest.main(['-m', '"not known_failing"', '-v'])
+    #errno = pytest.main()
     sys.exit(errno)
