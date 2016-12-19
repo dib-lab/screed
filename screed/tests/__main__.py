97fe19d43c9cc61432769bb193663e05d834d63b
Luiz Irber <luiz.irber@gmail.com>
2016-12-19 23:37:13 +0000

    Try to install pytest, if missing

--- screed/tests/__main__.py
+++ screed/tests/__main__.py
@@ -1,7 +1,15 @@
 import os
 import sys
 
+
 if __name__ == '__main__':
+    from setuptools import setup
+    setup_params = {
+        "setup_requires": ['pytest_runner'],
+        "tests_require": ['pytest'],
+    }
+    setup(**setup_params)
+
     import pytest
     errno = pytest.main(["-m not known_failing", '-v',
                          os.path.dirname(__file__)])
