4a6ca3943f6f94b7d311595e8494b6a9be119d93
Luiz Irber <luiz.irber@gmail.com>
2017-03-28 13:16:16 -0700

    versioneer update

--- screed/_version.py
+++ screed/_version.py
@@ -40,8 +40,8 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
-    cfg.tag_prefix = "'v'  # i.e. v1.2.0"
+    cfg.style = "git-describe"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "'.'"
     cfg.versionfile_source = "screed/_version.py"
     cfg.verbose = False
