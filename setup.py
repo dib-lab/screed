56f6f4bba60184012f54550984c63818a3d8ca16
Luiz Irber <luizirber@users.noreply.github.com>
2020-02-11 17:34:53 -0800

    Fix deprecation warning in screedRecord (#82)
    * setup updates for 1.0.1
    * fix deprecation warning in screedRecord
    * fix pep8

--- setup.py
+++ setup.py
@@ -13,12 +13,13 @@ import versioneer
 setup(name='screed',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
-      description='Screed is a biological sequence parsing and '
-                  'storage/retrieval library for DNA and protein sequences.',
+      description_file="README.md",
+      long_description_content_type="text/markdown",
       author='Luiz Irber, Peter Cock, Michael R. Crusoe, Jacob Fenton, '
-             'Thomas Fenzl, Sarah Guermond, Tim Head, Kevin D. Murray, '
-             'Alexander Nolley, Camille Scott, Daniel Standage, '
-             'Benjamin R. Taylor, Michael Wright, en zyme, C. Titus Brown',
+             'Thomas Fenzl, Sarah Guermond, Tim Head, Mark Mandel, '
+             'Kevin D. Murray, Alexander Nolley, Camille Scott, '
+             'Daniel Standage, Benjamin R. Taylor, Michael Wright, en zyme, '
+             'C. Titus Brown',
       author_email='ctbrown@ucdavis.edu',
       url='http://github.com/dib-lab/screed/',
       zip_safe=False,
