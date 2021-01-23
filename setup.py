07d91f1147268193ed3ca4e02aa5071f11478d3e
C. Titus Brown <titus@idyll.org>
2021-01-22 18:27:58 -0800

    start updating setup etc

--- setup.py
+++ setup.py
@@ -1,40 +1,11 @@
 #!/usr/bin/env python
 # Copyright (c) 2016, The Regents of the University of California.
-from __future__ import print_function
-try:
-    from setuptools import setup
-except ImportError:
-    print('(WARNING: importing distutils, not setuptools!)')
-    from distutils.core import setup
-
-import imp
-fp, pathname, description = imp.find_module('versioneer')
-versioneer = imp.load_module('versioneer', fp, pathname, description)
-del imp
-versioneer.VCS = 'git'
-versioneer.versionfile_source = 'screed/_version.py'
-versioneer.versionfile_build = 'screed/_version.py'
-versioneer.tag_prefix = 'v'  # i.e. v1.2.0
-versioneer.parentdir_prefix = '.'
-CMDCLASS = versioneer.get_cmdclass()
+from setuptools import setup
 
 setup(name='screed',
-      version=versioneer.get_version(),
-      description='A short read database',
-      author='Alex Nolley, C. Titus Brown',
-      author_email='ctbrown@ucdavis.edu',
-      url='http://github.com/dib-lab/screed/',
-      zip_safe=False,
-      include_package_data=True,
-      packages=['screed', 'screed.tests'],
-      package_data={
-          'screed.tests': ['test.*', 'test-whitespace.fa', 'empty.fa']},
-      license='BSD',
+#      packages=['screed', 'screed.tests'],
+#      package_data={
+#          'screed.tests': ['test.*', 'test-whitespace.fa', 'empty.fa']},
       setup_requires=['pytest-runner'],
       tests_require=['pytest >= 3.0', 'pytest-cov'],
-      cmdclass=versioneer.get_cmdclass(),
-      install_requires=['bz2file'],
-      entry_points={'console_scripts': [
-          'screed = screed.__main__:main'
-          ]
       })
