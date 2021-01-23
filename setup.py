ec4f62b42afbe775a9b6b346b33f8b87d81f6bf5
C. Titus Brown <titus@idyll.org>
2021-01-22 18:50:05 -0800

    remove versioneer

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
 # Copyright (c) 2016, The Regents of the University of California.
 from setuptools import setup
-
-setup(name='screed',
-#      packages=['screed', 'screed.tests'],
-#      package_data={
-#          'screed.tests': ['test.*', 'test-whitespace.fa', 'empty.fa']},
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest >= 3.0', 'pytest-cov'],
-      )
+setup()
